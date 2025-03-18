1d0a766ac1ee67b3dcd61201e7fd6c1979456535
Fábio <herogeekeduc01@gmail.com>
2025-03-18 20:14:16 -0300

    adicionando novos arquivos

---
+++ login.js
@@ -0,0 +1,12 @@
+function login(){
+    
+    let usuario = document.getElementById("usuario").value;
+    let senha = document.getElementById("senha").value;
+    //alert(usuario+senha);
+    if(usuario=="fabio" && senha=="123"){
+        window.location.href = "pagina2.html";
+    } else{
+        
+    }
+
+}
